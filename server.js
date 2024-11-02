dee750c698c90586c82fbe18b5ac24bf23347570
Vijaykumar M <55099188+vijayemk@users.noreply.github.com>
2024-11-02 22:45:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const express = require('express');
 const app = ex
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-const bodyparser = require('body-parser');
-const path = require('path');
+const bodyparser 
 
 dotenv.config({path : 'config.env'});
 const PORT = process.env.PORT || 8080
